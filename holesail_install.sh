798022048f52a49a7fa1eb8bdb0e709fd6e96f23
Walter Schoenly <wschoenly@tutanota.com>
2026-06-25 15:59:04 -0600

    updated

---
+++ holesail_install.sh
@@ -0,0 +1,6 @@
+wget -q https://github.com/holesail/holesail/releases/latest/download/linux-x64.zip -O holesail.zip && \
+unzip -o holesail.zip && \
+chmod +x holesail && \
+sudo mv holesail /usr/local/bin/ && \
+rm holesail.zip && \
+echo "Holesail installed successfully to /usr/local/bin/holesail"
